a95396058f98e70b039774eb6562248b4749fa1a
rweir4 <rebeccaweir12@gmail.com>
2023-06-02 09:20:41 -0400

    Initial Setup

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+// app.get('/api', (req, res) => {
+//   res.json({ 'users': ['userOne', 'userTwo', 'userThree'] })
+// })
+ 
+// server is on 5000, client is on 3000 because ruby
+// app.listen(5000, () => { console.log('Server started on port 5000') })
